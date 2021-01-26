7903ffb018492557eb56833a04c2200f2c256a3b
Julius Bacosa <juliusbacosa@gmail.com>
2021-01-26 14:00:57 +0800

    .

--- HelloApp/index.js
+++ HelloApp/index.js
@@ -2,10 +2,20 @@
 
 
 
+function GetNext(arr, i) {
+    console.log(arr[i]);
+    i++;
+    if(arr[i] != null){
+        GetNext(arr, i);
+    }
+}
+
 function ArrayChallenge(arr){
-    arr.forEach(element => {
-       console.log(element); 
-    });
+    // arr.forEach(element => {
+    //    console.log(element); 
+    // });
+    let i = 0;
+    GetNext(arr, i);
 }
 function main() {
  let arr = [1,2,3,4,5,6,7,8,9,10];   
